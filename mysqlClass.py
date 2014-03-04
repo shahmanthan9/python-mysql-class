3cdd7d286fa5e11ef444a3c5b35074010a645dcc
Manthan Shah <manthan@vivaconnect.co>
2014-03-04 20:00:17 +0530

    Changed exception handling while executing query so that it will return empty result set and will not stop execution of code

--- mysqlClass.py
+++ mysqlClass.py
@@ -1,3 +1,5 @@
+from pymysql.err import DatabaseError
+
 __author__ = 'shadow walker'
 import pymysql
 import pymysql.cursors
@@ -5,36 +7,40 @@ import pymysql.cursors
 
 class MySQL:
     def __init__(self, DBHOST, DBPORT, DBUSER, DBPASSWD, DBNAME):
+        self.conn = None
         try:
             self.conn = pymysql.connect(host=DBHOST, port=DBPORT, user=DBUSER, passwd=DBPASSWD, db=DBNAME,
                                         cursorclass=pymysql.cursors.DictCursor)
             self.conn.autocommit(1)
-        except self.conn.DatabaseError as exc:
+        except pymysql.DatabaseError as exc:
             print(exc)
-            exit
+            exit()
 
     def executeSQL(self, sql):
+        temp = []
+        count = 0
+        result = {}
+
         try:
             cur = self.conn.cursor()
             cur.execute(sql)
         except cur.DatabaseError as sql_exception:
             print(sql_exception)
-            exit()
+            result['rows'] = temp
+            result['count'] = count
+            return result
 
-        temp = []
-        result = {}
-
-        count = 0
         for row in cur:
-            temp.append(row)
-            count += 1
+                temp.append(row)
+                count += 1
         result['rows'] = temp
         result['count'] = count
         cur.close()
+
         return result
 
     def escapeString(self, arg):
-        return self.conn.escape_string(arg)
+        return self.conn.escape_string(str(arg))
 
     def lastInsertId(self):
         self.conn.insert_id()
